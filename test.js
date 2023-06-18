b7255c482eea95d99cda177a21923e7b53798c2b
swing <123>
2023-06-18 17:00:15 +0800

    沒做什麼

---
+++ test.js
@@ -0,0 +1,16 @@
+const mysql = require('mysql2');
+
+(async() => {
+    const pool = mysql.createPool({
+        host: 'localhost',
+        port: 3306,
+        user: 'root',
+        password: '000000',
+        database: 'test',
+        connectionLimit: 1
+    }).promise();
+    let data = (await pool.query('select * from testable;'))[0];
+    for(let i in data) {
+        console.log(data[i]);
+    }
+})();
